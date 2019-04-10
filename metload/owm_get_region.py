66d0811883637cafa81087c5862090809b97ffdc
will <dodge.ttu@gmail.com>
2019-04-10 08:24:37 -0500

    update

--- metload/owm_get_region.py
+++ metload/owm_get_region.py
@@ -1,10 +1,7 @@
 import requests
 from datetime import datetime as dt
-<<<<<<< HEAD
 
-=======
 from metload.sunrise_sunset import sun_rise_set
->>>>>>> e37ae9a73216b904bbc3d6f88d8398f75c672ef5
 
 def region_info(lat, lon, city_count, APPID, units):
     """ Return current weather observation data for a number of cities about an origin.
@@ -46,14 +43,11 @@ def parse_met_vars(owm_data):
     count = owm_data['count'] # Number of cities in data
     met = owm_data['list'] # Weather data for all cites
 
-<<<<<<< HEAD
-=======
     # Calculation for sunrise and sunset is includes here based on the center point for the region.
     tdy_sunrise, tdy_sunset = sun_rise_set(latitude=33.576698, longitude=-101.855072, UTC_offset=5)
     tdy_sunrise = tdy_sunrise.timestamp()
     tdy_sunset = tdy_sunset.timestamp()
 
->>>>>>> e37ae9a73216b904bbc3d6f88d8398f75c672ef5
     cln_obs = {}
 
     for st in met:
@@ -63,11 +57,8 @@ def parse_met_vars(owm_data):
         st_main = st['main']
         st_dt = st['dt']
         st_wind = st['wind']
-<<<<<<< HEAD
         st_sys = st['sys'] # OWM internal parameters, but sunrise and sunset is included here
-=======
         # st_sys = st['sys'] # OWM internal parameters
->>>>>>> e37ae9a73216b904bbc3d6f88d8398f75c672ef5
         st_rain = st['rain']
         st_snow = st['snow']
         st_clouds = st['clouds']
@@ -81,13 +72,10 @@ def parse_met_vars(owm_data):
             'site_name': st_name if st_name else 'NaN',
             'lat': st_coords['lat'] if st_coords else 'NaN',
             'lon': st_coords['lon'] if st_coords else 'NaN',
-<<<<<<< HEAD
             'sunrise':st_sys['sunrise'] if ('sunrise' in st_sys) else 'NaN',
             'sunset':st_sys['sunset'] if ('sunset' in st_sys) else 'Nan',
-=======
             'sunrise':tdy_sunrise if tdy_sunrise else 'NaN',
             'sunset':tdy_sunset if tdy_sunset else 'Nan',
->>>>>>> e37ae9a73216b904bbc3d6f88d8398f75c672ef5
             'temp': st_main['temp'] if 'temp' in st_main else 'NaN',
             'pressure': st_main['pressure'] if 'pressure' in st_main else 'NaN',
             'humidity': st_main['humidity'] if 'humidity' in st_main else 'NaN',
@@ -97,13 +85,10 @@ def parse_met_vars(owm_data):
             'wind_speed': st_wind['speed'] if 'speed' in st_wind else 'NaN',
             'wind_dir': st_wind['deg'] if 'deg' in st_wind else 'NaN',
             'wind_gust': st_wind['gust'] if 'gust' in st_wind else 'NaN',
-<<<<<<< HEAD
             'rain_1h': st_rain['1h'] if st_rain else 0,
             'rain_3h': st_rain['3h'] if st_rain else 0,
-=======
             'rain_1h': st_rain['1h'] if st_rain and ('1h' in st_rain) else 0,
             'rain_3h': st_rain['3h'] if st_rain and ('3h' in st_rain) else 0,
->>>>>>> e37ae9a73216b904bbc3d6f88d8398f75c672ef5
             'snow': st_snow if st_snow else 0,
             'weather_id': st_weather[0]['id'] if 'id' in st_weather[0] else 'NaN',
             'weather_main': st_weather[0]['main'] if 'main' in st_weather[0] else 'NaN',
